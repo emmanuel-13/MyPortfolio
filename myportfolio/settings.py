3dc72d8b692c7cdb9deec6aa13670f82a7b54e15
Master Emmanuel <jackoliver023@gmail.com>
2022-12-23 16:35:04 +0100

    update settings

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+emkd&rzosilsv@7qhpv6m%#dd-s!k2(49oap(*4z*dex_7-vf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
